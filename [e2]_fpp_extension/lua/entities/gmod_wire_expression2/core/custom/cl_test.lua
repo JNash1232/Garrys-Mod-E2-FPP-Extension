f65b855d385f7fb3f62f94bcb44171f00b3443fb
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2021-02-19 06:06:55 +0000

    asd

---
+++ [e2]_fpp_extension/lua/entities/gmod_wire_expression2/core/custom/cl_test.lua
@@ -0,0 +1,5 @@
+E2Helper.Descriptions["physgunShare"] = "shares physgun prop protection with E, equivalent to ticking the 'Physgun buddy' box in the buddies menu"
+E2Helper.Descriptions["gravgunShare"] = "shares gravgun prop protection with E, equivalent to ticking the 'Gravgun buddy' box in the buddies menu"
+E2Helper.Descriptions["useShare"] = "shares use prop protection with E, equivalent to ticking the 'Use buddy' box in the buddies menu"
+E2Helper.Descriptions["damageShare"] = "shares damage entity prop protection with E, equivalent to ticking the 'Entity damage buddy' box in the buddies menu"
+E2Helper.Descriptions["toolgunShare"] = "shares toolgun prop protection with E, equivalent to ticking the 'Toolgun buddy' box in the buddies menu"
---
+++ [e2]_fpp_extension/lua/entities/gmod_wire_expression2/core/custom/test.lua
@@ -0,0 +1,36 @@
+--[[
+    Lets see if this works
+    -- Jason1232
+]]
+
+E2Lib.RegisterExtension("fpp", false, "an extension that allows you to use e2 to add people to you falcos prop protection buddies list.")
+
+e2function string ohCock()
+    return "This is actually working, im a freaking genius."
+end
+
+--- this is a test to see if this works as a description
+e2function void entity:physgunShare(number active)
+    if not IsValid(this) and not this:IsPlayer() then return nil end
+    this.SharePhysgun1 = (active == 1)
+end
+
+e2function void entity:gravgunShare(number active)
+    if not IsValid(this) and not this:IsPlayer() then return nil end
+    this.ShareGravgun1 = (active == 1)
+end
+
+e2function void entity:useShare(number active)
+    if not IsValid(this) and not this:IsPlayer() then return nil end
+    this.SharePlayerUse1 = (active == 1)
+end
+
+e2function void entity:damageShare(number active)
+    if not IsValid(this) and not this:IsPlayer() then return nil end
+    this.ShareEntityDamage1 = (active == 1)
+end
+
+e2function void entity:toolgunShare(number active)
+    if not IsValid(this) and not this:IsPlayer() then return nil end
+    this.ShareToolgun1 = (active == 1)
+end
