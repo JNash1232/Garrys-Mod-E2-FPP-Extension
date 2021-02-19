220caa71623c89c9f87900d77c3977cc60bb223e
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2021-02-19 07:18:25 +0000

    added share for each type and some comments

--- [e2]_fpp_extension/lua/entities/gmod_wire_expression2/core/custom/test.lua
+++ [e2]_fpp_extension/lua/entities/gmod_wire_expression2/core/custom/test.lua
@@ -1,37 +1,40 @@
 --[[
     Lets see if this works
-    -- Jason1232
+    -- Jason1232 & KieranFYI
 ]]
 
+
+--- Include the fpp buddies library needed to actually add buddies
+include("fpp/client/buddies.lua")
+
+--- Register this extension, give it a name and desc and disabled by default
 E2Lib.RegisterExtension("fpp", false, "an extension that allows you to use e2 to add people to you falcos prop protection buddies list.")
 
-e2function string ohCock()
-    return "This is actually working, im a freaking genius."
+--- physgun share
+e2function void entity:physgunShare(number active)
+    if not IsValid(this) or not this:IsPlayer() then return nil end
+    FPP.SaveBuddy(this:SteamID(), "Physgun", "physgun", active)
 end
 
---- this is a test to see if this works as a description
-e2function string entity:physgunShare(number active)
-    this.SharePhysgun1 = 1
-    recalculateCanTouch(player.GetAll(), {this})
-    return "testytest2"
-end
-
+--- gravgun share
 e2function void entity:gravgunShare(number active)
-    if not IsValid(this) and not this:IsPlayer() then return nil end
-    this.ShareGravgun1 = (active == 1)
+    if not IsValid(this) or not this:IsPlayer() then return nil end
+    FPP.SaveBuddy(this:SteamID(), "Gravgun", "gravgun", active)
 end
 
+--- use share
 e2function void entity:useShare(number active)
-    if not IsValid(this) and not this:IsPlayer() then return nil end
-    this.SharePlayerUse1 = (active == 1)
+    if not IsValid(this) or not this:IsPlayer() then return nil end
+    FPP.SaveBuddy(this:SteamID(), "Use", "playeruse", active)
 end
 
+--- entity damage share
 e2function void entity:damageShare(number active)
-    if not IsValid(this) and not this:IsPlayer() then return nil end
-    this.ShareEntityDamage1 = (active == 1)
+    if not IsValid(this) or not this:IsPlayer() then return nil end
+    FPP.SaveBuddy(this:SteamID(), "Entity damage", "entitydamage", active)
 end
 
 e2function void entity:toolgunShare(number active)
-    if not IsValid(this) and not this:IsPlayer() then return nil end
-    this.ShareToolgun1 = (active == 1)
+    if not IsValid(this) or not this:IsPlayer() then return nil end
+    FPP.SaveBuddy(this:SteamID(), "Toolgun", "toolgun", active)
 end
