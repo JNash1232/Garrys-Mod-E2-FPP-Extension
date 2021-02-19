367c7e30c846eee260f5b03f054d693dc0c49320
Kieran <github@andersons.bz>
2021-02-19 07:39:20 +0000

    Formatting

--- lua/entities/gmod_wire_expression2/core/custom/test.lua
+++ lua/entities/gmod_wire_expression2/core/custom/test.lua
@@ -12,30 +12,45 @@ E2Lib.RegisterExtension("fpp", false, "an extension that allows you to use e2 to
 
 --- physgun share
 e2function void entity:sharePhysgun(number active)
-    if not IsValid(this) or not this:IsPlayer() then return nil end
-    FPP.SaveBuddy(this:SteamID(), "Physgun", "physgun", active)
+    if not IsValid(this) or not this:IsPlayer() then
+		return nil
+	end
+
+    FPP.SaveBuddy(this:SteamID(), "Physgun", "physgun", (active == 1))
 end
 
 --- gravgun share
 e2function void entity:shareGravgun(number active)
-    if not IsValid(this) or not this:IsPlayer() then return nil end
-    FPP.SaveBuddy(this:SteamID(), "Gravgun", "gravgun", active)
+    if not IsValid(this) or not this:IsPlayer() then
+		return nil
+	end
+
+    FPP.SaveBuddy(this:SteamID(), "Gravgun", "gravgun", (active == 1))
 end
 
 --- use share
 e2function void entity:shareUse(number active)
-    if not IsValid(this) or not this:IsPlayer() then return nil end
+    if not IsValid(this) or not this:IsPlayer() then
+		return nil
+	end
+
     FPP.SaveBuddy(this:SteamID(), "Use", "playeruse", active)
 end
 
 --- entity damage share
 e2function void entity:shareDamage(number active)
-    if not IsValid(this) or not this:IsPlayer() then return nil end
+    if not IsValid(this) or not this:IsPlayer() then
+		return nil
+	end
+
     FPP.SaveBuddy(this:SteamID(), "Entity damage", "entitydamage", active)
 end
 
 --- toolgun share
 e2function void entity:shareToolgun(number active)
-    if not IsValid(this) or not this:IsPlayer() then return nil end
+    if not IsValid(this) or not this:IsPlayer() then
+		return nil
+	end
+	
     FPP.SaveBuddy(this:SteamID(), "Toolgun", "toolgun", active)
 end
