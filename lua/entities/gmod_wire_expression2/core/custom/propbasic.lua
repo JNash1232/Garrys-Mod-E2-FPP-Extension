86d4138cc31dfa0316d787b5dbb7bce435b86de9
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2021-02-20 05:28:21 +0000

    added 3 function
    added:
    
    E:propFreeze()
    E:propNotSolid()
    E:propIsSolid()
    
    all 3 work fine in my sandbox.

--- lua/entities/gmod_wire_expression2/core/custom/propbasic.lua
+++ lua/entities/gmod_wire_expression2/core/custom/propbasic.lua
@@ -6,3 +6,36 @@
 
 --- Register this extension, give it a name and desc and disabled by default
 E2Lib.RegisterExtension("propbasic", false, "a basic alternative to propcore")
+
+--- sets the cost
+__e2setcost(50)
+
+
+--- propfreeze, 1 = frozen, 0 = unfrozen, pretty much same as propcore
+e2function void entity:propFreeze(number frozen)
+    if not IsValid(this) or this:IsPlayer() then
+        return nil
+    end
+
+    this:GetPhysicsObject():EnableMotion(frozen <= 0)
+end
+
+--- propsolid sets the solidity of the prop 
+e2function void entity:propNotSolid(number solid)
+    if not IsValid(this) or this:IsPlayer() then
+        return nil
+    end
+    
+    this:SetSolid(solid >= 1 and 6 or 0)
+end
+
+--- gets the solidity of the prop
+e2function number entity:propIsSolid()
+    if not IsValid(this) or this:IsPlayer() then
+        return nil
+    end
+    
+    local solid = this:GetSolid() and 1 or 0
+    return solid
+end
+
