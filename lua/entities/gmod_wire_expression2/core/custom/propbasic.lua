9461d278dd34d4d2926ae625871883f7201af31e
Kieran <github@andersons.bz>
2021-02-21 11:46:48 +0000

    Wake physics when changing physics properties
    House keeping

--- lua/entities/gmod_wire_expression2/core/custom/propbasic.lua
+++ lua/entities/gmod_wire_expression2/core/custom/propbasic.lua
@@ -1,8 +1,6 @@
 --[[
-    Lets see if this works
     -- Jason1232 & KieranFYI
 ]]
-
 
 --- Register this extension, give it a name and desc and disabled by default
 E2Lib.RegisterExtension("propbasic", false, "a basic alternative to propcore")
@@ -10,63 +8,58 @@ E2Lib.RegisterExtension("propbasic", false, "a basic alternative to propcore")
 --- sets the cost
 __e2setcost(50)
 
-
 --- propfreeze, 1 = frozen, 0 = unfrozen, pretty much same as propcore
 e2function void entity:propFreeze(number frozen)
-    if not IsValid(this) or this:IsPlayer() then
-        return nil
+	if not IsValid(this) or this:IsPlayer() 
+		or not this:CPPICanPhysgun(self.player) then
+		return nil
     end
-    
-    local canTool = this:CPPICanPhysgun(self.player)
-    if canTool then
-        this:GetPhysicsObject():EnableMotion(frozen <= 0)
-    end
+
+	local phys = this:GetPhysicsObject()
+	phys:EnableMotion(frozen <= 0)
+	phys:Wake()
 end
 
 --- sets gravity
 e2function void entity:enableGravity(number active)
-    if not IsValid(this) or this:IsPlayer() then
-        return nil
-    end
-    
-    local canTool = this:CPPICanTool(self.player, "toolgun")
-    if canTool then
-        this:GetPhysicsObject():EnableGravity(active >= 1)
-    end
-end
+	if not IsValid(this) or this:IsPlayer() 
+		or not this:CPPICanTool(self.player, "toolgun") then
+		return nil
+	end
 
---- propsolid sets the solidity of the prop 
-e2function void entity:propNotSolid(number solid)
-    if not IsValid(this) or this:IsPlayer() then
-        return nil
-    end
-
-    local canTool = this:CPPICanTool(self.player, "toolgun")
-    if canTool then
-        this:SetSolid(solid >= 1 and 0 or 6)
-    end
-end
-
---- drawShadow sets if E should draw shadows
-e2function void entity:drawShadow(number draw)
-    if not IsValid(this) or this:IsPlayer() then
-        return nil
-    end
-
-    local canTool = this:CPPICanTool(self.player, "toolgun")
-    if canTool then
-        this:DrawShadow(draw >= 1)
-    end
+	local phys = this:GetPhysicsObject()
+	phys:EnableGravity(active >= 1)
+	phys:Wake()
 end
 
 --- enableDrag ... it enables/disbales drag
 e2function void entity:enableDrag(number active)
-    if not IsValid(this) or this:IsPlayer() then
-        return nil
-    end
+    if not IsValid(this) or this:IsPlayer() 
+		or not this:CPPICanTool(self.player, "toolgun") then
+		return nil
+	end
 
-    local canTool = this:CPPICanTool(self.player, "toolgun")
-    if canTool then
-        this:GetPhysicsObject():EnableDrag(active >= 1)
-    end
+	local phys = this:GetPhysicsObject()
+	phys:EnableDrag(active >= 1)
+	phys:Wake()
+end
+
+--- propsolid sets the solidity of the prop 
+e2function void entity:propNotSolid(number solid)
+	if not IsValid(this) or this:IsPlayer() 
+		or not this:CPPICanTool(self.player, "toolgun") then
+		return nil
+	end
+
+    this:SetSolid(solid >= 1 and 0 or 6)
+end
+
+--- drawShadow sets if E should draw shadows
+e2function void entity:drawShadow(number draw)
+    if not IsValid(this) or this:IsPlayer() 
+		or not this:CPPICanTool(self.player, "toolgun") then
+		return nil
+	end
+
+    this:DrawShadow(draw >= 1)
 end
