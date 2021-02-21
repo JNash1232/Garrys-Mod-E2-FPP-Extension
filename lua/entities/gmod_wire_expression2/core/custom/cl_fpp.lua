33e17d5b2655b5541f9026fb3a280f70bead479a
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2021-02-21 13:09:31 +0000

    Update cl_fpp.lua

--- lua/entities/gmod_wire_expression2/core/custom/cl_fpp.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_fpp.lua
@@ -1,6 +1,16 @@
 E2Helper.Descriptions["sharePhysgun"] = "shares physgun prop protection with E where E is a player, otherwise shares physgun prop protection with everyone for E"
+E2Helper.Descriptions["canPhysgun"] = "returns 1 if player E has shared physgun with you or if E not a player returns 1 when E is shared with you"
+
 E2Helper.Descriptions["shareGravgun"] = "shares toolgun prop protection with E where E is a player, otherwise shares toolgun prop protection with everyone for E"
+E2Helper.Descriptions["canGravgun"] = "returns 1 if player E has shared gravgun with you or if E not a player returns 1 when E is shared with you"
+
 E2Helper.Descriptions["shareUse"] = "shares use prop protection with E where E is a player, otherwise shares use prop protection with everyone for E"
+E2Helper.Descriptions["camUse"] = "returns 1 if player E has shared use with you or if E not a player returns 1 when E is shared with you"
+
 E2Helper.Descriptions["shareDamage"] = "shares damage entity prop protection with E where E is a player, otherwise shares damage entity prop protection with everyone for E"
+E2Helper.Descriptions["canDamage"] = "returns 1 if player E has shared damage entity with you or if E not a player returns 1 when E is shared with you"
+
 E2Helper.Descriptions["shareToolgun"] = "shares toolgun prop protection with E where E is a player, otherwise shares toolgun prop protection with everyone for E"
+E2Helper.Descriptions["canToolgun"] = "returns 1 if player E has shared toolgun with you or if E not a player returns 1 when E is shared with you"
+
 E2Helper.Descriptions["share"] = "shares all protection with target E where E is a player, otherwise shares all prop protection with everyone for E"
